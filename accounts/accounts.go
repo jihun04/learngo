23656b7b9f099b1749e354f5416d171d8df7b9a2
jihun04 <yujihun20060217@gmail.com>
2022-02-08 15:25:39 +0900

    Methods part Two

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,7 @@
 package accounts
 
+import "errors"
+
 // Account struct
 type Account struct {
 	owner   string
@@ -13,11 +15,27 @@ func NewAccount(owner string) *Account {
 }
 
 // Deposit x amount on your account
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) error {
+	if amount < 0 {
+		return errors.New("can't deposit. amount must be more than 0")
+	}
 	a.balance += amount
+	return nil
 }
 
 // Return balance
-func (a Account) GetBalance() int {
+func (a *Account) GetBalance() int {
 	return a.balance
+}
+
+// Withdraw balance
+func (a *Account) Withdraw(amount int) error {
+	if amount < 0 {
+		return errors.New("can't withdraw. amount must be more than 0")
+	}
+	if amount >= a.balance {
+		return errors.New("can't. you're too poor to withdraw as that amount")
+	}
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"learngo/accounts"
+	"log"
 )
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
 
 func main() {
 	account := accounts.NewAccount("jihun")
-	account.Deposit(10)
+	checkErr(account.Deposit(10))
+	checkErr(account.Withdraw(11))
 	fmt.Println(account.GetBalance())
 }
